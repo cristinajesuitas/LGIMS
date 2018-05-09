6aa929100b67b5d5fbc46519086d2863c3ccee2c
cristinajesuitas <cristinajesuitas@users.noreply.github.com>
2018-05-09 16:07:03 +0800

    Add files via upload

---
+++ User.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\components;
+
+use Yii;
+
+/**
+ * Extended yii\web\User
+ *
+ * This allows us to do "Yii::$app->user->something" by adding getters
+ * like "public function getSomething()"
+ *
+ * So we can use variables and functions directly in `Yii::$app->user`
+ */
+class User extends \yii\web\User
+{
+    public function getUsername()
+    {
+        return \Yii::$app->user->identity->username;
+    }
+
+    public function getName()
+    {
+        return \Yii::$app->user->identity->fname;
+    }
+}
