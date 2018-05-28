7157bc10dbf0780f6f42cb0dd578962638a59d3f
cristinajesuitas <cristinajesuitas@users.noreply.github.com>
2018-05-28 17:04:57 +0800

    Create _form.php

---
+++ backend/views/Lgulevel/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
+use backend\models\Lgulevel;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Lgulevel */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="lgulevel-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'code')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
