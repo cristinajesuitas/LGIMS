e2309941c98c7da5f72c2a7a7b99406d2d72a027
cristinajesuitas <cristinajesuitas@users.noreply.github.com>
2018-05-28 17:19:56 +0800

    Create _form.php

---
+++ backend/views/province/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\models\Region;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Province */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="province-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'cap_region_id')->dropDownList( ArrayHelper::map(Region::find()->all(), 'id', 'name'), ['prompt'=>'Choose Region']) ?> 
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'code')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
